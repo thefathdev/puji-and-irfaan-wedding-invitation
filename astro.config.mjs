91853b12ea4533309ab11a256bca10c94d286a8c
Fath Irfaan <abdillahfathu@gmail.com>
2024-11-29 17:34:17 +0700

    fix: change deployment to cloudflare page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
 });
